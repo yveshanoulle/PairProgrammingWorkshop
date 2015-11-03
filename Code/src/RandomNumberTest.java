e5e531de85269ccb36b7263d35d9d8479389326e
yveshanoulle <mailing@hanoulle.be>
2015-11-03 19:16:59 +0100

    two green test

--- Code/src/RandomNumberTest.java
+++ Code/src/RandomNumberTest.java
@@ -4,10 +4,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import sun.nio.cs.Surrogate.Generator;
+
+
+// Write a program that generates 100 random numbers between 1000 and
+// 9999. Among the generated numbers find and display the number, which
+// are "axially symmetric", which means that their records are of type XYYX
+
+
 public class RandomNumberTest {
 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
@@ -15,9 +24,20 @@ public class RandomNumberTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	
+	public void Contains_Hunderd_Numbers() {
+		producer myProducer = new producer();
+		
+		assertEquals(100, myProducer.count());
+		
 	}
 
+	@Test
+	public void Is_Bigger_Then_1000() {
+		producer myProducer = new producer();
+		
+		assertTrue(myProducer.number(0)>1000);
+		
+	}
+
+	
 }
